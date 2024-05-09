c38bc8712ed27ef0bd679f846df4aa75a76fc270
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-05-10 01:57:22 +0530

    Update HyperlaneMessageSender.sol

--- contracts/HyperlaneMessageSender.sol
+++ contracts/HyperlaneMessageSender.sol
@@ -5,13 +5,24 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 import "@hyperlane-xyz/core/interfaces/IMailbox.sol";
+import "@hyperlane-xyz/core/interfaces/IInterchainSecurityModule.sol";
 
 contract HyperlaneMessageSender {
     IMailbox outbox;
     event SentMessage(uint32 destinationDomain, bytes32 recipient, string message);
 
+    IInterchainSecurityModule public interchainSecurityModule;
+
+    function setInterchainSecurityModule(address _module) public {
+        interchainSecurityModule = IInterchainSecurityModule(_module);
+    }
+
     constructor(address _outbox) {
         outbox = IMailbox(_outbox);
+    }
+
+    function addressToBytes32(address _addr) external pure returns (bytes32) {
+    return bytes32(uint256(uint160(_addr)));
     }
 
     function sendString(
